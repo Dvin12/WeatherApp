84093acbceadfe99c137122758ca258eddeb4c56
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-02-10 19:11:01 +0200

    added query selectors

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("#search-form");
+const input = document.querySelector("#serach");
+const list = document.querySelector(".info");
+
+const apiKey = "df64afae23fae2a3b41d8fb056651f17";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
