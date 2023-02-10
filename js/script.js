b11c15dfe6cf5b7e05711f47ee28665351f810a7
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-02-10 19:28:10 +0200

    added search functionality

--- js/script.js
+++ js/script.js
@@ -6,4 +6,36 @@ const apiKey = "df64afae23fae2a3b41d8fb056651f17";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  let inputVal = input.ariaValueMax;
+
+  const listItemsArray = Array.from(list.querySelectorAll(".info li"));
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter((el) => {
+      let content = "";
+      let cityName = el
+        .querySelector(".info__description__conditions__city")
+        .textContent.toLowerCase();
+      let cityCountry = el
+        .querySelector(".info__description__conditions__city__country")
+        .textContent.toLowerCase();
+
+      if (inputVal.includes(",")) {
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = input.split(",")[0];
+
+          content = cityName;
+        } else {
+          content = `${cityName}, ${cityCountry}`;
+        }
+      } else {
+        content = cityName;
+      }
+
+      return content == inputVal.toLowerCase();
+    });
+  }
+
+  const url = "https://api";
 });
