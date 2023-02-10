6b9ddd9b376eaa8b96e4cf42dbf66cfb13d7d02a
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-02-10 20:01:28 +0200

    added card functionality, fetching data from the api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("#search-form");
-const input = document.querySelector("#serach");
+const input = document.querySelector(".header__form input");
 const list = document.querySelector(".info");
 
 const apiKey = "df64afae23fae2a3b41d8fb056651f17";
@@ -7,7 +7,7 @@ const apiKey = "df64afae23fae2a3b41d8fb056651f17";
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let inputVal = input.ariaValueMax;
+  let inputVal = input.value;
 
   const listItemsArray = Array.from(list.querySelectorAll(".info li"));
 
@@ -37,5 +37,33 @@ form.addEventListener("submit", (e) => {
     });
   }
 
-  const url = "https://api";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const { main, name, sys, weather } = data;
+
+      const icon = `../svg/${weather[0]["icon"]}.svg`;
+
+      const li = document.createElement("li");
+
+      const markup = `
+      <img src="${icon}" alt="${weather[0]["description"]}" />
+      <div class="info__description">
+        <h2>${Math.round(main.temp)} <sup>&deg; C</sup></h2>
+        <p class="info__description__conditions">${weather[0][
+          "description"
+        ].toUpperCase()}</p>
+        <h3 class="info__description__conditions__city">
+          ${name}
+          <span class="info__description__conditions__city__country"
+            >${sys.country}</span
+          >
+        </h3>
+      </div>`;
+
+      li.innerHTML = markup;
+      list.appendChild(li);
+    });
 });
